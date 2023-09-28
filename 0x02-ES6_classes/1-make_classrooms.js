c55c8205216b2cf6939156ebda932a93ef9c9602
EnGentech <engen.inyang@gmail.com>
2023-09-28 23:06:36 +0100

    modified and fix bugs

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,6 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  let check = [new ClassRoom(9), new ClassRoom(20), new ClassRoom(34)];
+  const check = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
   return check;
 }
