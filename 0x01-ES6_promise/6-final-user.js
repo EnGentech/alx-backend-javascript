71dfdd06e42d1f101fca8383f24e82de49b0ca77
EnGentech <engen.inyang@gmail.com>
2023-09-28 10:03:13 +0100

    fix bug

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((val) => {
+    .then((val) => 
       val.map((condition) => ({
         status: condition.status,
-        value: condition.status === 'fulfilled' ? condition.value : String(condition.reason),
-      }));
-    });
+        value: condition.status === 'fulfilled' ? condition.value : condition.reason.toString(),
+      })));
 }
