6980114f1323acf3c0d9017cbb4250222afb2cb5
EnGentech <engen.inyang@gmail.com>
2023-09-29 00:47:03 +0100

    ordinary method and static method implemented

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,33 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(worth) {
+    this._amount = worth;
+  }
+
+  set currency(dol) {
+    this._currency = dol;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    const newAmount = amount * conversionRate;
+    return newAmount;
+  }
+}
