1659d91added84af40560318cb33be1c37a99269
EnGentech <engen.inyang@gmail.com>
2023-10-03 19:01:59 +0100

    create typed array

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,12 +1,3 @@
-export default function updateStudentGradeByCity(student, address, grade) {
-  const studentsData = student.filter((byCity) => byCity.location === address);
+import createInt8TypedArray from './5-typed_arrays.js';
 
-  const updatedGrade = studentsData.map((newgrade) => {
-    const findGrade = grade.find((allGrades) => allGrades.studentId === newgrade.id);
-    if (findGrade) {
-      return { ...newgrade, grade: findGrade.grade };
-    }
-    return { ...newgrade, grade: 'N/A' };
-  });
-  return updatedGrade;
-}
+console.log(createInt8TypedArray(10, 2, 89));
